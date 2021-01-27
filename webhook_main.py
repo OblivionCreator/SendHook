da336cdd34210653582ec142b8f48c57a3f1538f
OblivionCreator <oblivioncreator@outlook.com>
2021-01-27 08:39:25 +0000

    Updated code - Removed redundant lines and un-commented delete code.

--- webhook_main.py
+++ webhook_main.py
@@ -1,12 +1,8 @@
-import discord
 from discord.ext import commands
-from discord import Webhook, AsyncWebhookAdapter
 import aiohttp
 
 bot = commands.Bot(
-command_prefix=['.'],
-case_insensitive=True
-)
+command_prefix=['.'])
 
 try:
     with open('token.txt') as file:
@@ -14,16 +10,7 @@ try:
         file.close()
 except(FileNotFoundError, IOError):
     print("Unable to start! There is no token!")
-    exit(99)
-
-try:
-    with open(".config") as file:
-        file.close()
-except (FileNotFoundError, IOError):
-    print("No Config file found! A blank config file has been generated.")
-
-    file = open('.config', 'x')
-    file.close()
+    exit(-1)
 
 @bot.command()
 async def send(ctx, name, *, message:str):
@@ -37,7 +24,7 @@ async def send(ctx, name, *, message:str):
 
     await sendWH(name=name, img=img, message=message, channel=channel, author=ctx.author)
     try:
-        #await ctx.message.delete()
+        await ctx.message.delete()
         pass
     except Exception as e:
         print(f"Unable to delete message due to exception:\n{e}")
@@ -45,11 +32,11 @@ async def send(ctx, name, *, message:str):
 
 async def sendWH(name, img, message, channel, author):
 
-    webhook = await channel.create_webhook(name='NoWebhooks Generated Webhook', reason=f'{author} ({author.id}) used NoWebhooks to send a message in {channel}')
+    webhook = await channel.create_webhook(name='SendHook Generated Webhook', reason=f'{author} ({author.id}) used NoWebhooks to send a message in {channel}')
 
     async with aiohttp.ClientSession() as session:
-        await webhook.send(message, username=name, avatar_url=img)
-        await webhook.delete(reason="Auto-Delete Used Webhook")
+        await webhook.send(message, username=name[0:80], avatar_url=img)
+        await webhook.delete(reason="SendHook - Auto-Delete Used Webhook")
 
 
 bot.run(token)
